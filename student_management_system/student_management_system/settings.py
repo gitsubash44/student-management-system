e177b730abced2a801bee41d9568b2eebd41afec
gitsubash44 <dhamisubash866@gmail.com>
2025-02-08 10:40:39 +0545

    adding front-end part for system

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
+STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR / 'static'
 # Application definition
 
 INSTALLED_APPS = [
